d2553f7a4cc4ffdd338a96e9cce7cdd4a4e0b6a1
Juan Jose Sepulveda <juan.sepulveda@edu.uag.mx>
2023-02-19 18:42:02 -0700

    fix multiple swipper problem, now I can add multiple swipper on the same page without problems, but the content.html is broken now

--- src/setup-swipper.js
+++ src/setup-swipper.js
@@ -1,74 +1,87 @@
-var swiper = new Swiper(".mySwiper", {
-    //slidesPerView: 5,
-    //spaceBetween: 30,
-    //pagination: {
-    //    el: ".swiper-pagination",
-    //clickable: true,
-    //},
-    lazyPreloaderClass: "swiper-lazy-preloader",
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    freeMode: true,
-    // Responsive breakpoints
-    breakpoints: {
-        // when window width is >= 320px
-        320: {
-            slidesPerView: 2,
-            spaceBetween: 24,
-            navigation: {
-                enabled: false,
-            }
+// Instance swipper with the id 0
+var swiper0 = mainSwiperCreate(0)
+hideNavigationsButtons(swiper0)
+
+var swiper1 = mainSwiperCreate(1)
+hideNavigationsButtons(swiper1)
+
+/**
+ * 
+ */
+function mainSwiperCreate(id = 0) {
+    let swiper = new Swiper(`.mySwiper${id}`, {
+        lazyPreloaderClass: `swiper${id}-lazy-preloader`,
+        navigation: {
+            nextEl: `.swiper${id}-button-next`,
+            prevEl: `.swiper${id}-button-prev`,
         },
-        // when window width is >= 576px
-        576: {
-            slidesPerView: 2,
-            spaceBetween: 24,
-            navigation: {
-                enabled: false,
-            }
-        },
-        // when window width is >= 768px
-        768: {
-            slidesPerView: 3,
-            spaceBetween: 24,
-            navigation: {
-                enabled: true,
-            }
-        },
-        // when window width is >= 992px
-        992: {
-            slidesPerView: 4,
-            spaceBetween: 24,
-            navigation: {
-                enabled: true,
-            }
-        },
-        // when window width is >= 1200px
-        1200: {
-            slidesPerView: 5,
-            spaceBetween: 24,
-            navigation: {
-                enabled: true,
+        freeMode: true,
+        // Responsive breakpoints
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+                slidesPerView: 2,
+                spaceBetween: 24,
+                navigation: {
+                    enabled: false,
+                }
+            },
+            // when window width is >= 576px
+            576: {
+                slidesPerView: 2,
+                spaceBetween: 24,
+                navigation: {
+                    enabled: false,
+                }
+            },
+            // when window width is >= 768px
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 24,
+                navigation: {
+                    enabled: true,
+                }
+            },
+            // when window width is >= 992px
+            992: {
+                slidesPerView: 4,
+                spaceBetween: 24,
+                navigation: {
+                    enabled: true,
+                }
+            },
+            // when window width is >= 1200px
+            1200: {
+                slidesPerView: 5,
+                spaceBetween: 24,
+                navigation: {
+                    enabled: true,
+                }
             }
         }
-    }
-});
+    });
 
-// I will place this here, to hidde the left navigration button at the beginning
-swiper.navigation.$prevEl.css('display', 'none');
+    return swiper
+}
 
-// Show/hide navigation buttons on slide change
-swiper.on('slideChange', function () {
-    if (swiper.isEnd) {
-        swiper.navigation.$nextEl.css('display', 'none');
-    } else {
-        swiper.navigation.$nextEl.css('display', 'block');
-    }
-    if (swiper.isBeginning) {
-        swiper.navigation.$prevEl.css('display', 'none');
-    } else {
-        swiper.navigation.$prevEl.css('display', 'block');
-    }
-});
+/**
+ * 
+ */
+function hideNavigationsButtons(swipper_instance) {
+    // I will place this here, to hidde the left navigration button at the beginning
+    swipper_instance.navigation.$prevEl.css('display', 'none');
+
+    // Show/hide navigation buttons on slide change
+    swipper_instance.on('slideChange', function () {
+        if (swipper_instance.isEnd) {
+            swipper_instance.navigation.$nextEl.css('display', 'none');
+        } else {
+            swipper_instance.navigation.$nextEl.css('display', 'block');
+        }
+        if (swipper_instance.isBeginning) {
+            swipper_instance.navigation.$prevEl.css('display', 'none');
+        } else {
+            swipper_instance.navigation.$prevEl.css('display', 'block');
+        }
+    });
+}
